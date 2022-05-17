230d20f5004ca22d585bb8bce07722d97c98b393
Lawrence LCI <lcheunglci@gmail.com>
2022-05-16 23:36:18 -0700

    Add PointOfInterestDto and use it in CityDto

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -12,11 +12,35 @@ namespace CityInfo.API
             // init dummy data
             Cities = new List<CityDto>()
             {
-                new CityDto() { Id = 1, Name ="New York City", Description="The one with that big park"},
-                new CityDto() { Id = 2, Name ="Toyko", Description="The one with that big kaiju"},
-                new CityDto() { Id = 3, Name ="Paris", Description="The one with that big tower"}
+                new CityDto() { Id = 1, Name ="New York City", Description="The one with that big park", PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto() {
+                        Id = 1, Name = "Central Park", Description = "The most visited urban park in the United States."
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2, Name = "Empire State building", Description="A 102-story skyscraper located in midtown Manhattan."
+                    }
+                } },
+                new CityDto() { Id = 2, Name ="Toyko", Description="The one with that big kaiju", PointsOfInterest = new List<PointOfInterestDto>() {
+                    new PointOfInterestDto() {
+                        Id = 1, Name = "Shibuya", Description = "The one with the crosswalk that works in every direction"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2, Name = "Ebisu", Description = "The one with the Museum of Yebisu Beer."
+                    }
+                } },
+                new CityDto() { Id = 3, Name ="Paris", Description="The one with that big tower", PointsOfInterest = new List<PointOfInterestDto>() {
+                    new PointOfInterestDto() {
+                        Id = 1, Name = "Eiffle tower", Description = "The tall pointy thing "
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2, Name = "Louvre Museum", Description="Looks like an apple store in Mission Impossible."
+                    }
+                } }
             };
         }
-
     }
 }
