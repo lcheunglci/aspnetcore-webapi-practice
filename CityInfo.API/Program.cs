b502c862849e5a55cefd57185e3691d7c2c29f72
Lawrence LCI <lcheunglci@gmail.com>
2022-05-19 02:09:15 -0700

    Add nlog as the log provider to log to file

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,34 +1,58 @@
 using Microsoft.AspNetCore.Mvc.Formatters;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddMvc().AddMvcOptions(
-    options =>
-    {
-        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-    });
-//.AddNewtonsoftJson(
-//    options =>
-//    {
-//        if (options.SerializerSettings.ContractResolver != null)
-//        {
-//            var castedResolver = options.SerializerSettings.ContractResolver as DefaultContractResolver;
-//            castedResolver.NamingStrategy = null;
-//        }
-//    });
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("initialize main");
 
-
-var app = builder.Build();
-
-app.UseRouting();
-
-app.UseEndpoints(endpoints =>
+try
 {
-    endpoints.MapControllers();
-});
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+    builder.Services.AddMvc().AddMvcOptions(
+        options =>
+        {
+            options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        });
+    //.AddNewtonsoftJson(
+    //    options =>
+    //    {
+    //        if (options.SerializerSettings.ContractResolver != null)
+    //        {
+    //            var castedResolver = options.SerializerSettings.ContractResolver as DefaultContractResolver;
+    //            castedResolver.NamingStrategy = null;
+    //        }
+    //    });
 
-app.UseStatusCodePages();
 
-app.MapGet("/", () => "Hello World!");
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
 
-app.Run();
+    var app = builder.Build();
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+
+    app.UseStatusCodePages();
+
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    // NLog: catch setup errors
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
