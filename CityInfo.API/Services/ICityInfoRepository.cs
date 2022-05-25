7335ff5b3eacae8c1feba2eea32c0d4a7914f921
Lawrence LCI <lcheunglci@gmail.com>
2022-05-24 23:57:39 -0700

    Add CityInfoRepository

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+
+        City GetCity(int cityId, bool includePointsOfInterest);
+
+        IEnumerable<PointOfInterest> GetPointOfInterestsForCity(int cityId, bool includePointsOfInterest);
+
+        PointOfInterest GetPointsOfInterestForCity(int cityId);
+
+
+    }
+}
