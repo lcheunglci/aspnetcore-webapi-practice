b1f14f2c063447fa8471a3d4525b19af090cb4d7
Lawrence LCI <lcheunglci@gmail.com>
2022-05-30 00:08:51 -0700

    Add the scope for dbcontext connection string, and add first migration

--- CourseLibrary.API/Program.cs
+++ CourseLibrary.API/Program.cs
@@ -1,13 +1,45 @@
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+builder.Services.AddDbContext<CourseLibraryContext>(options =>
+{
+    options.UseSqlServer(
+        @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetService<CourseLibraryContext>();
+        // for demo purposes, delete the database & migrate on startup so 
+        // we can start with a clean slate
+        context.Database.EnsureDeleted();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
