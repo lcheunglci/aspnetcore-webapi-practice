b274fe666ab6ad5304392ea2f24152d9dfc928c8
Lawrence LCI <lcheunglci@gmail.com>
2022-06-02 20:57:23 -0700

    Add HttpGet for a single Course for Author

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -29,8 +29,23 @@ namespace CourseLibrary.API.Controllers
 
             var coursesForAuthorFromRepo = _courseLibraryRepository.GetCourses(authorId);
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
-
         }
 
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid guid)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, guid);
+
+            if (courseForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
+        }
     }
 }
