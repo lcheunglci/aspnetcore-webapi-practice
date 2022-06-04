d695b027499b2da12bae73d5d831dcee871dfc96
Lawrence LCI <lcheunglci@gmail.com>
2022-06-04 11:17:59 -0700

    Extend the create author to allow courses to be added in a single request

--- CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -6,6 +6,6 @@
         public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string MainCategory { get; set; }
-
+        public ICollection<CourseForCreationDto> Courses { get; set; } = new List<CourseForCreationDto>();
     }
 }
