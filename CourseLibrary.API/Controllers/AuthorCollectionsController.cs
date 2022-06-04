29bed9c2e757b454c99ae3adf57574e66fdb5319
Lawrence LCI <lcheunglci@gmail.com>
2022-06-04 11:25:31 -0700

    Add endpoint to add a collection of authors in a single request

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorForCreations)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorForCreations);
+            foreach (var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+            _courseLibraryRepository.Save();
+
+            return Ok();
+        }
+    }
+}
