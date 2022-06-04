a97c7429817e1ddfdeed4e9361ccc47ea18efde4
Lawrence LCI <lcheunglci@gmail.com>
2022-06-03 23:20:36 -0700

    Add post endpoint for creating new authors

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -42,5 +42,22 @@ namespace CourseLibrary.API.Controllers
             return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
         }
 
+
+        [HttpPost]
+        public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+        {
+            var authorEntity = _mapper.Map<Entities.Author>(author);
+            _courseLibraryRepository.AddAuthor(authorEntity);
+            _courseLibraryRepository.Save();
+
+            var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+            return CreatedAtRoute("GetAuthor", new
+            {
+                authorId = authorToReturn.Id
+            },
+            authorToReturn);
+
+        }
+
     }
 }
---
+++ CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseLibrary.API.Models
+{
+    public class AuthorForCreationDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string MainCategory { get; set; }
+
+    }
+}
