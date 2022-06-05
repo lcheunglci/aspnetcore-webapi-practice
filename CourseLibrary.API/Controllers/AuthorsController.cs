3b2c2b13f1e8d75594f3757e35d6bcf79457b16d
Lawrence LCI <lcheunglci@gmail.com>
2022-06-05 01:05:58 -0700

    Add Options to the Authors endpoint

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -59,5 +59,12 @@ namespace CourseLibrary.API.Controllers
 
         }
 
+        [HttpOptions]
+        public IActionResult GetAuthorsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+            return Ok();
+        }
+
     }
 }
