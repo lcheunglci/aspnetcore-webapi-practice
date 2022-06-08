c679376bc4e402cb96a52b4fcf70e338ddeb15e0
Lawrence LCI <lcheunglci@gmail.com>
2022-06-08 01:12:02 -0700

    Add delete endpoint to the author endpoint

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -66,5 +66,23 @@ namespace CourseLibrary.API.Controllers
             return Ok();
         }
 
+        [HttpDelete("{authorId}")]
+        public ActionResult DeleteAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+
+            _courseLibraryRepository.Save();
+
+            return NoContent();
+        }
+
+
     }
 }
