139bfdb4af124af141a7e0768df94ca461fcfdf4
Lawrence LCI <lcheunglci@gmail.com>
2022-06-26 23:59:27 -0700

    Add InitialMigration

--- Books.API/Program.cs
+++ Books.API/Program.cs
@@ -1,4 +1,27 @@
+using Books.API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration["ConnectionStrings:BooksDBConnectionString"];
+builder.Services.AddDbContext<BookContext>(o => o.UseSqlServer(connectionString));
+
+builder.Services.Configure<ApplicationBuilder>(config =>
+{
+
+    if (!builder.Environment.IsProduction())
+    {
+        config.UseDeveloperExceptionPage();
+    }
+
+    config.UseHttpsRedirection();
+
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
