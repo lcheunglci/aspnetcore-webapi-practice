569840c70f4abffded68f79cfd7a01c7f6a4822a
Lawrence LCI <lcheunglci@gmail.com>
2022-06-27 23:56:41 -0700

    Add async get resource

---
+++ Books.API/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Books.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository booksRepository)
+        {
+            _bookRepository = booksRepository ??
+                throw new ArgumentNullException(nameof(booksRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var bookEntities = await _bookRepository.GetBooksAsync();
+            return Ok(bookEntities);
+
+        }
+    }
+}
