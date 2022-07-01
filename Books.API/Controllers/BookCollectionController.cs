cd916420c83989180503117c37f36ae20375a690
Lawrence LCI <lcheunglci@gmail.com>
2022-07-01 01:04:06 -0700

    Add bookcollection controller for bulk creation of books

---
+++ Books.API/Controllers/BookCollectionController.cs
@@ -0,0 +1,25 @@
+﻿using Books.API.Models;
+using Books.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [Route("api/bookcollection")]
+    [ApiController]
+    public class BookCollectionController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookCollectionController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository ?? throw new ArgumentNullException(nameof(bookRepository));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBookCollection(
+            IEnumerable<BookForCreation> bookCollection)
+        {
+            return Ok();
+        }
+    }
+}
