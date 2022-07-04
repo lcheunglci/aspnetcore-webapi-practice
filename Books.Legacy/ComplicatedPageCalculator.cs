613f6c68871a1e3b61c3ef03a48a43e60b5578f8
Lawrence LCI <lcheunglci@gmail.com>
2022-07-04 00:35:42 -0700

    Add book legacy project to simulate long loading running cpu bound code

---
+++ Books.Legacy/ComplicatedPageCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Books.Legacy
+{
+    public class ComplicatedPageCalculator
+    {
+        /// <summary>
+        /// Full CPU load for 5 seconds
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateBookPage()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            while (true)
+            {
+                if (watch.ElapsedMilliseconds > 5000)
+                {
+                    break;
+                }
+            }
+
+            return 42;
+        }
+
+
+    }
+}
