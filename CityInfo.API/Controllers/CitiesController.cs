81399babf47ae830a835e2a4014bac026b3c9553
Lawrence LCI <lcheunglci@gmail.com>
2026-05-12 23:43:43 -0700

    feat: add cities controller

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+	[ApiController]
+	public class CitiesController : ControllerBase
+	{
+		[HttpGet("api/cities")]
+		public JsonResult GetCtities()
+		{
+			return new JsonResult(new []
+			{
+				new
+				{
+					Id = 1,
+					Name = "New York City",
+					Description = "The one with that big park."
+				},
+				new
+				{
+					Id = 2,
+					Name = "Tokyo",
+					Description = "The land of the rising sun."
+				},
+				new
+				{
+					Id = 3,
+					Name = "Paris",
+					Description = "The one with that big tower."
+				}
+			});
+		}
+	}
+}
