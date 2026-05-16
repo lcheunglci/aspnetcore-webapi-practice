cbe2bc81ced78b882bff41c3e86d0aaa222a59ce
Lawrence LCI <lcheunglci@gmail.com>
2026-05-15 23:41:31 -0700

    feat: add file endpoint

---
+++ CityInfo.API/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+	[Route("api/files")]
+	[ApiController]
+	public class FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider) : ControllerBase
+	{
+		[HttpGet("{fileId}")]
+		public ActionResult GetFile(string fileId)
+		{
+			// look up the actual file, depending on the fileId
+			// demo code
+			var pathToFile = "sample-file.pdf";
+
+			// check whether the file exists
+			if (!System.IO.File.Exists(pathToFile))
+			{
+				return NotFound();
+			}
+
+			if (!fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+			{
+				contentType = "application/octet-stream";
+			}
+
+			var bytes = System.IO.File.ReadAllBytes(pathToFile);
+			return File(bytes, contentType, Path.GetFileName(pathToFile));
+		}
+	}
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -11,8 +13,9 @@ builder.Services.AddProblemDetails(options => options.CustomizeProblemDetails = 
 {
 	ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
 	ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
-
 });
+
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
