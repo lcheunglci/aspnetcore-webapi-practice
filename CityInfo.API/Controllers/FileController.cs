8a5edab277c0991858ce295c55a85de3d672747e
Lawrence LCI <lcheunglci@gmail.com>
2026-05-18 23:05:54 -0700

    feat: add file upload support

--- CityInfo.API/Controllers/FileController.cs
+++ CityInfo.API/Controllers/FileController.cs
@@ -28,5 +28,30 @@ namespace CityInfo.API.Controllers
 			var bytes = await System.IO.File.ReadAllBytesAsync(pathToFile);
 			return File(bytes, contentType, Path.GetFileName(pathToFile));
 		}
+
+		[HttpPost]
+		public async Task<ActionResult> CreateFile(IFormFile file)
+		{
+			// Validate the input. Put a limit on filesize to avoid large uploads attacks.
+			// Only accept .pdf files (check content-type)
+			if (file.Length is 0
+				|| file.Length > 20 * 1024 * 1024 // 20 MB limit
+				|| file.ContentType != "application/pdf"
+				) 
+			{
+				return BadRequest("No file or an invalid one has been inputted.");
+			}
+
+			// Create a tile path. Avoid using file.FileName, as an attack can provide a 
+			// malicious file name with path traversal characters (e.g. ../../evil-file.pdf) to overwrite important files on the server.
+			// including full paths or relative paths.
+
+			var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+			await using var stream = new FileStream(path, FileMode.Create);
+			await file.CopyToAsync(stream);
+
+			return Ok("Your file has been uploaded successfully.");
+		}
 	}
 }
