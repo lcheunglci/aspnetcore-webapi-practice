61ed6b84ff426c8f987278e0b8f14d828d24bf15
Lawrence LCI <lcheunglci@gmail.com>
2026-05-24 23:14:49 -0700

    feat: add migration

---
+++ Books.API/DbContexts/BooksContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Books.API.DbContexts;
+
+public class BooksContextFactory : IDesignTimeDbContextFactory<BooksContext>
+{
+    public BooksContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BooksContext>();
+        optionsBuilder.UseSqlite("Data Source=books.db"); // Update with your connection string if needed
+        return new BooksContext(optionsBuilder.Options);
+    }
+}
