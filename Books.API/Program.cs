9e36535c54a4c4183dac073e35f8d343b0dcd32b
Lawrence LCI <lcheunglci@gmail.com>
2026-05-26 23:15:32 -0700

    feat: add minimal api books endpoint

--- Books.API/Program.cs
+++ Books.API/Program.cs
@@ -1,5 +1,8 @@
-﻿using Books.API.DbContexts;
+﻿using AutoMapper;
+using Books.API.DbContexts;
+using Books.API.Models;
 using Books.API.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,5 +26,19 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("api/minimal/books", async (IBooksRepository repo, IMapper mapper) =>
+{
+	var bookEntities = await repo.GetBooksAsync();
+	return Results.Ok(mapper.Map<IEnumerable<BookDto>>(bookEntities));
+});
+
+app.MapGet("api/minimal/books/{id:guid}", async Task<Results<Ok<BookDto>, NotFound>> (Guid id, IBooksRepository repo, IMapper mapper) =>
+{
+	var bookEntity = await repo.GetBookAsync(id);
+	if (bookEntity == null)
+		return TypedResults.NotFound();
+	return TypedResults.Ok(mapper.Map<BookDto>(bookEntity));
+});
 
 app.Run();
