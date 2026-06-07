0bed711dbf8f52695e63dbe027049b919241a066
Lawrence LCI <lcheunglci@gmail.com>
2026-06-06 23:33:09 -0700

    Add unit testing project for Books.API

---
+++ Books.API.Test/BooksRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Books.API.Test
+{
+	public class BooksRepositoryTests
+	{
+		// sync test: public void TestName() { ... }
+
+		[Fact]
+		public async Task GetBooksAsync_ReturnsBooks()
+		{
+			// Arrange
+			var repository = new FakeBooksRepository();
+			// Act
+			var books = await repository.GetBooksAsync(CancellationToken.None);
+			// Assert
+			Assert.NotNull(books);
+			Assert.NotEmpty(books);
+		}
+
+		[Fact]
+		public async Task GetBookAsync_WithValidId_ReturnsCorrectBook()
+		{
+			// Arrange
+			var repo = new FakeBooksRepository();
+			var bookId = Guid.Parse("2A9E0CD7-8214-4B44-A443-A5CD5E561FA5");
+			// Act 
+			var book = await repo.GetBookAsync(bookId, CancellationToken.None);
+			// Assert
+			Assert.NotNull(book);
+			Assert.Equal("The Winds of Winter", book.Title);
+		}
+
+		// Sync assertion: Assert.Throws<T>(() => Method());
+		// Async assertion: await Assert.ThrowsAsync<T>(() => MthodAsync());
+		[Fact]
+		public async Task GetBooksAsync_WithCancellationToken_ThrowsOperationCanceledException()
+		{
+			// Arrange
+			var repo = new FakeBooksRepository();
+			var cancelledToken = new CancellationToken(canceled: true);
+			// Act & Assert
+			await Assert.ThrowsAnyAsync<OperationCanceledException>(
+				() => repo.GetBooksAsync(cancelledToken));
+		}
+	}
+}
---
+++ Books.API.Test/FakeBooksRepository.cs
@@ -0,0 +1,69 @@
+﻿using Books.API.Entities;
+using Books.API.Models.External;
+using Books.API.Services;
+
+namespace Books.API.Test
+{
+	public class FakeBooksRepository : IBooksRepository
+	{
+		private readonly List<Book> _books = [
+			new(
+				// Guid.Parse("5b1c2b4d"), 
+				Guid.Parse("2A9E0CD7-8214-4B44-A443-A5CD5E561FA5"),
+				Guid.Parse("45F128D0-A460-49D8-AC83-3D009EF51C22"),
+				"The Winds of Winter",
+				"The book that seems impossible to write."
+				),
+			new (
+				// Guid.Parse("d8663e5e"),
+				Guid.Parse("859ACB2E-F79F-4B62-9F54-1D14B111ABC7"),
+				Guid.Parse("45F128D0-A460-49D8-AC83-3D009EF51C22"),
+				"A Game of Thrones",
+				"A Game of Thrones is the first novel of A Song of Ice and Fire."
+				)
+			];
+
+		public void AddBook(Book bookToAdd)
+		{
+			_books.Add(bookToAdd);
+		}
+
+		public Task<Book?> GetBookAsync(Guid id, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			return Task.FromResult(_books.FirstOrDefault(b => b.Id == id));
+		}
+
+		public Task<BookCoverDto?> GetBookCoverAsync(string id, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<BookCoverDto>> GetBookCoverOneByOneAsync(Guid bookId, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<BookCoverDto>> GetBookCoversAfterWaitForAllAsync(Guid id, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Book>> GetBooksAsync(CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			return Task.FromResult<IEnumerable<Book>>(_books);
+		}
+
+		public IAsyncEnumerable<Book> GetBooksAsyncEnumerable()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			return Task.FromResult(true);
+		}
+	}
+}
