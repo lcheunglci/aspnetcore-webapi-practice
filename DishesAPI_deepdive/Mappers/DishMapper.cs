9f1050d86547738cf63d0962139b58615eccbc60
Lawrence LCI <lcheunglci@gmail.com>
2026-06-10 23:14:35 -0700

    Add DishMapper and update project dependencies
    Added `Riok.Mapperly` and `Scalar.AspNetCore` to `DishesAPI.csproj` for object mapping. Introduced `DishMapper` class to handle entity-to-DTO mappings, including methods for `Dish` and `Ingredient` transformations. Encapsulated mapping logic with partial methods and attributes to manage property exclusions and circular references.

---
+++ DishesAPI_deepdive/Mappers/DishMapper.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.NetworkInformation;
+using DishesAPI.Entities;
+using DishesAPI.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace DishesAPI.Mappers
+{
+	[Mapper]
+	public static partial class DishMapper
+	{
+		[MapperIgnoreSource(nameof(Dish.Ingredients))]
+		public static partial DishDto ToDishDto(this Dish dish);
+
+		public static partial IEnumerable<DishDto> ToDishDtoList(this IEnumerable<Dish> dishes);
+
+		[MapperIgnoreTarget(nameof(Dish.Id))]
+		[MapperIgnoreTarget(nameof(Dish.Ingredients))]
+		public static partial Dish ToDish(this DishForCreationDto dishForCreationDto);
+
+		[MapperIgnoreTarget(nameof(Dish.Id))]
+		[MapperIgnoreTarget(nameof(Dish.Ingredients))]
+
+		public static partial void UpdateFromDto(
+			this DishForUpdateDto dishForUpdateDto, Dish dish);
+
+		[MapperIgnoreSource(nameof(Ingredient.Dishes))]
+		[MapperIgnoreTarget(nameof(IngredientDto.DishId))]
+		public static partial IngredientDto MapIngredient(Ingredient ingredient);
+
+		public static IngredientDto ToIngredientDto(
+			this Ingredient ingredient, Guid dishId)
+		{
+			var dto = MapIngredient(ingredient);
+			dto.DishId = dishId;
+			return dto;
+
+		}
+
+		public static IEnumerable<IngredientDto> ToIngredientDtoList(
+			this IEnumerable<Ingredient> ingredients, Guid dishId)
+		{
+			return ingredients.Select(i => i.ToIngredientDto(dishId));
+		}
+
+		public static Dish ToDish(this DishForCreationDto dishForCreationDto)
+		{
+			return new Dish
+			{
+				Name = dishForCreationDto.Name
+			};
+		}
+
+		public static void UpdateFromDto(this Dish dish, DishForUpdateDto dishForUpdateDto)
+		{
+			dish.Name = dishForUpdateDto.Name;
+
+		}
+
+		[MapperIgnoreSource(nameof(Ingredient.Dishes))]
+		[MapperIgnoreTarget(nameof(IngredientDto.DishId))]
+		private static partial IngredientDto MapIngredient(Ingredient ingredient);
+
+		public static IngredientDto ToIngredientDto(
+			this Ingredient ingredient, Guid dishId
+			)
+		{
+			var dto = MapIngredient(ingredient);
+			dto.DishId = dishId;
+			return dto;
+		}
+
+		public static IEnumerable<IngredientDto> ToIntegrientDtoList(
+			this IEnumerable<Ingredient> ingredients, Guid dishId)
+		{
+			return ingredients.Select(i => i.ToIngredientDto(dishId));
+		}
+
+	}
+}
