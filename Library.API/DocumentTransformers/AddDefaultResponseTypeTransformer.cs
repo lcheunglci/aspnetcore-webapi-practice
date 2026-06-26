dec069e287ad89f3b0c55734ed3ae63ef2e4d040
Lawrence LCI <lcheunglci@gmail.com>
2026-06-25 23:59:23 -0700

    Add AddDefaultResponseTypeTransformer stub class
    Added AddDefaultResponseTypeTransformer.cs with a new class implementing IOpenApiDocumentTransformer. The TransformAsync method is stubbed and throws NotImplementedException. Included required using directives and namespace.

---
+++ Library.API/DocumentTransformers/AddDefaultResponseTypeTransformer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace Library.API.DocumentTransformers
+{
+	public class AddDefaultResponseTypeTransformer : IOpenApiDocumentTransformer
+	{
+		public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
