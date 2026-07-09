50a5b94f138a976cbce2f237886fbb4d67f74434
Lawrence LCI <lcheunglci@gmail.com>
2026-07-08 17:20:29 -0700

    Add support for author collection routes with array binding
    Introduced ArrayModelBinder for binding array parameters from route values. Added GET endpoint to AuthorCollectionsController for retrieving multiple authors by IDs. Updated POST endpoint to return 201 Created with location header referencing the new GET route.

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,26 @@ namespace CourseLibrary.API.Controllers
 			_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 		}
 
+
+
+		[HttpGet("({authorIds})")]
+		public async Task<ActionResult<IEnumerable<AuthorForCreationDto>>>
+			GetAuthorCollection(
+			[ModelBinder(BinderType = typeof(ArrayModelBinder))]
+			[FromRoute] IEnumerable<Guid> authorIds)
+		{
+			var authorEntities = await _courseLibraryRepository.GetAuthorsAsync(authorIds);
+			if (authorIds.Count() != authorEntities.Count())
+			{
+				return NotFound();
+			}
+
+			// map
+			var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+			return Ok(authorsToReturn);
+		}
+
+
 		[HttpPost]
 		public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(
 			IEnumerable<AuthorForCreationDto> authorCollection)
@@ -30,8 +51,13 @@ namespace CourseLibrary.API.Controllers
 			}
 			await _courseLibraryRepository.SaveAsync();
 
-			return Ok();
+			var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
 
+			var authorIdsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+
+			return CreatedAtRoute("GetAuthorCollection", 
+				new { authorIds = authorIdsAsString }, 
+				authorCollectionToReturn);
 		}
 	}
 }
