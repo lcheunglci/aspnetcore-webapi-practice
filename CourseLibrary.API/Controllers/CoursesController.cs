0f2ae3f3a795fb38f86e095135c8c17096b57e0c
Lawrence LCI <lcheunglci@gmail.com>
2026-07-09 23:21:39 -0700

    Update course update flow to use CourseForUpdateDto
    Refactored UpdateCourseForAuthor to accept CourseForUpdateDto instead of CourseDto. Added CourseForUpdateDto class and mapping to Entities.Course in CoursesProfile for improved separation of concerns and validation.

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -69,7 +69,7 @@ public class CoursesController(ICourseLibraryRepository courseLibraryRepository,
     [HttpPut("{courseId}")]
     public async Task<IActionResult> UpdateCourseForAuthor(Guid authorId,
       Guid courseId,
-      CourseDto course)
+      CourseForUpdateDto course)
     {
         if (!await _courseLibraryRepository.AuthorExistsAsync(authorId))
         {
