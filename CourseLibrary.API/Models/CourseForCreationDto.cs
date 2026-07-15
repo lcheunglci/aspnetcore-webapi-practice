f646ff0cabebc81178ca42c31b146f29f8aac11d
Lawrence LCI <lcheunglci@gmail.com>
2026-07-14 22:54:10 -0700

    Add custom error messages to CourseForCreationDto validation
    Updated the [Required] and [MaxLength] attributes on Title and Description properties to include custom error messages, improving validation feedback for users.

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -4,9 +4,9 @@ namespace CourseLibrary.API.Models;
 
 public class CourseForCreationDto
 {
-	[Required]
-	[MaxLength(100)]
+	[Required(ErrorMessage = "You should fill out a title.")]
+	[MaxLength(100, ErrorMessage = "The title should not have more than 100 characters.")]
     public string Title { get; set; } = string.Empty;
-	[MaxLength(1000)]
+	[MaxLength(1000, ErrorMessage = "The description should not have more than 1000 characters.")]
     public string? Description { get; set; } = string.Empty;
 }
