691bef33faf898acc73594491ac75ab9fa06f1c5
Lawrence LCI <lcheunglci@gmail.com>
2026-08-12 20:37:07 -0700

    Add EmployeeFactory unit tests for employee creation
    Added EmployeeFactoryTests class in EmployeeManagement.Test with four unit tests. These tests verify correct construction of InternalEmployee and ExternalEmployee objects and validate the Salary property values and ranges.

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+	public class EmployeeFactoryTests
+	{
+		[Fact]
+		public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+		{
+			// Arrange
+			var employeeFactory = new EmployeeFactory();
+
+			// Act
+			var employee = (InternalEmployee)employeeFactory.CreateEmployee("John", "Doe");
+
+			// Assert
+			Assert.Equal(2500, employee.Salary);
+		}
+
+		[Fact]
+		public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3000()
+		{
+			// Arrange
+			var employeeFactory = new EmployeeFactory();
+
+			// Act
+			var employee = (InternalEmployee)employeeFactory.CreateEmployee("John", "Doe");
+
+			// Assert
+			Assert.InRange(employee.Salary, 2500, 3000);
+		}
+
+		[Fact]
+		public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeExternalEmployee()
+		{
+			// Arrange
+			var employeeFactory = new EmployeeFactory();
+
+			// Act
+			var employee = employeeFactory.CreateEmployee("John", "Doe", "Marvin", true);
+
+			// Assert
+			Assert.IsType<ExternalEmployee>(employee);
+		}
+
+
+		[Fact]
+		public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeInternalEmployee()
+		{
+			// Arrange
+			var employeeFactory = new EmployeeFactory();
+
+			// Act
+			var employee = employeeFactory.CreateEmployee("John", "Doe");
+
+			// Assert
+			Assert.IsType<InternalEmployee>(employee);
+		}
+	}
+}
