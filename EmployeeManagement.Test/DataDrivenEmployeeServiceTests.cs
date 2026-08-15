ad3bd96c3fd168063732d183ff16f4c0ec2901a6
Lawrence LCI <lcheunglci@gmail.com>
2026-08-14 23:27:53 -0700

    Add CSV-driven parameterized tests for EmployeeService
    Added GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithFileData test using data from EmployeeServiceTestData.csv. Introduced StronglyTypedEmployeeServiceTestData_FromFile for reading CSV test cases. Updated EmployeeManagement.Test.csproj to copy CSV file to output. Added EmployeeServiceTestData.csv with test cases.

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -55,5 +55,27 @@ namespace EmployeeManagement.Test
 			// Assert
 			Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
 		}
+
+		[Theory]
+		[ClassData(typeof(StronglyTypedEmployeeServiceTestData_FromFile))]
+		public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithFileData(
+			int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+		{
+			// Arrange
+			var employeeManagementRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+			employeeManagementRepositoryMock.Setup(m => m.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+			var employeeService = new EmployeeService(employeeManagementRepositoryMock.Object, new EmployeeFactory());
+
+			var internalEmployee = new InternalEmployee("John", "Doe", 5, 3000, false, 1);
+
+
+			// Act
+			await employeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+			// Assert
+			Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+		}
 	}
 }
---
+++ EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestData_FromFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Test.TestData
+{
+	public class StronglyTypedEmployeeServiceTestData_FromFile : TheoryData<int, bool>
+	{
+		public StronglyTypedEmployeeServiceTestData_FromFile()
+		{
+			var testDataLines = File.ReadAllLines("TestData/EmployeeServiceTestData.csv");
+
+			foreach (var line in testDataLines)
+			{
+				var splitString = line.Split(',');
+				if (int.TryParse(splitString[0], out int raise)
+					&& bool.TryParse(splitString[1], out bool minimumRaiseGiven))
+				{
+					Add(raise, minimumRaiseGiven);
+				}
+			}
+		}
+
+	}
+}
