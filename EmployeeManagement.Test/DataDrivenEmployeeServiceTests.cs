d407ee588841795244960ab3a4f3606cebe9deb6
Lawrence LCI <lcheunglci@gmail.com>
2026-08-14 23:22:43 -0700

    Add strongly-typed test data for employee service tests
    Introduced StronglyTypedEmployeeServiceTestData for parameterized
    tests in EmployeeManagement.Test.TestData. Updated
    DataDrivenEmployeeServiceTests.cs to use [Theory] and [ClassData]
    with the new test data class for data-driven testing of
    GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithInClassData.

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using EmployeeManagement.Business;
 using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.DataAccess.Services;
+using EmployeeManagement.Test.TestData;
 using Moq;
 
 namespace EmployeeManagement.Test
@@ -14,6 +15,28 @@ namespace EmployeeManagement.Test
 		[InlineData(100, true)]
 		[InlineData(200, false)]
 		public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithInlineData(
+			int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+		{
+			// Arrange
+			var employeeManagementRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+			employeeManagementRepositoryMock.Setup(m => m.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+			var employeeService = new EmployeeService(employeeManagementRepositoryMock.Object, new EmployeeFactory());
+
+			var internalEmployee = new InternalEmployee("John", "Doe", 5, 3000, false, 1);
+
+
+			// Act
+			await employeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+			// Assert
+			Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+		}
+
+		[Theory]
+		[ClassData(typeof(StronglyTypedEmployeeServiceTestData))]
+		public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithInClassData(
 			int raiseGiven, bool expectedValueForMinimumRaiseGiven)
 		{
 			// Arrange
---
+++ EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Test.TestData
+{
+	public class StronglyTypedEmployeeServiceTestData : TheoryData<int, bool>
+	{
+		public StronglyTypedEmployeeServiceTestData()
+		{
+			Add(100, true);
+			Add(200, false);
+		}
+	}
+}
