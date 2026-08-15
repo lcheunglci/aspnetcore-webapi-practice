aff903954f20d5880d8c33a796475ff59d63cfb5
Lawrence LCI <lcheunglci@gmail.com>
2026-08-14 23:19:38 -0700

    Add data-driven tests for EmployeeService GiveRaiseAsync
    Added DataDrivenEmployeeServiceTests using xUnit [Theory] and [InlineData] to test GiveRaiseAsync in EmployeeService. Verified MinimumRaiseGiven for various raise amounts.

---
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
+using Moq;
+
+namespace EmployeeManagement.Test
+{
+	public class DataDrivenEmployeeServiceTests
+	{
+		[Theory]
+		[InlineData(100, true)]
+		[InlineData(200, false)]
+		public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue_WithInlineData(
+			int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+		{
+			// Arrange
+			var employeeManagementRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+			employeeManagementRepositoryMock.Setup(m => m.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+			var employeeService = new EmployeeService(employeeManagementRepositoryMock.Object, new EmployeeFactory());
+
+			var internalEmployee = new InternalEmployee("John", "Doe", 5, 3000, false, 1);
+
+
+			// Act
+			await employeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+			// Assert
+			Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+		}
+	}
+}
