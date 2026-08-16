aa8d6cbffbe30b21458951cb718349b332145280
Lawrence LCI <lcheunglci@gmail.com>
2026-08-15 23:14:29 -0700

    Add unit test for SuggestedBonus calculation
    Added FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated to EmployeeServiceTests.cs to verify SuggestedBonus is 400 for internal employees with two attended courses using Moq.

--- EmployeeManagement.Test/EmployeeServiceTests.cs
+++ EmployeeManagement.Test/EmployeeServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using EmployeeManagement.Business;
+﻿using EmployeeManagement.Business;
 using EmployeeManagement.Business.Exceptions;
 using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.DataAccess.Services;
@@ -25,5 +22,35 @@ namespace EmployeeManagement.Test
 			await Assert.ThrowsAsync<EmployeeInvalidRaiseException>(
 				async () => await employeeService.GiveRaiseAsync(internalEmployee, 50));
 		}
+
+
+		[Fact]
+		public async Task FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated()
+		{
+			// Arrange
+			var employeeManagementRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+			employeeManagementRepositoryMock.Setup(
+				m => m.GetInternalEmployeeAsync(It.IsAny<Guid>()))
+				.ReturnsAsync(new InternalEmployee("Tony", "Hall", 2, 2500, false, 2)
+				{
+					AttendedCourses = new List<Course>
+				{
+					new Course("A course"),
+					new Course("Another course")
+				}
+				});
+
+			var employeeService = new EmployeeService(employeeManagementRepositoryMock.Object, new EmployeeFactory());
+
+			// Act
+			var employee = await employeeService.FetchInternalEmployeeAsync(Guid.Empty);
+
+			// Assert
+			Assert.NotNull(employee);
+			Assert.Equal(400, employee.SuggestedBonus);
+		}
+
+
 	}
 }
