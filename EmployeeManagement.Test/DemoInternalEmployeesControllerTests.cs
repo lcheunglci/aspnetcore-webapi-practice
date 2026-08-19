89c11b2bcf8129ff08a9ac589fa81d5025d2d7a3
Lawrence LCI <lcheunglci@gmail.com>
2026-08-18 23:47:28 -0700

    Add unit test for invalid model in controller action
    Added DemoInternalEmployeesControllerTests with a test verifying that CreateInternalEmployee returns BadRequestObjectResult and SerializableError when given an invalid model. Used Moq for dependencies and AutoMapper for mapping.

---
+++ EmployeeManagement.Test/DemoInternalEmployeesControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.MapperProfiles;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace EmployeeManagement.Test
+{
+	public class DemoInternalEmployeesControllerTests
+	{
+		[Fact]
+		public async Task CreateInternalEmployee_InvalidModel_MustReturnBadRequest()
+		{
+			// Arrange
+			var employeeServiceMock = new Mock<IEmployeeService>();
+			var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<EmployeeProfile>(), new LoggerFactory());
+
+			var mapper = new Mapper(mapperConfiguration);
+
+			var controller = new DemoInternalEmployeesController(employeeServiceMock.Object, mapper);
+
+			// Act
+			var result = await controller.CreateInternalEmployee(new InternalEmployeeForCreationDto());
+
+			// Assert
+			var actionResult = Assert.IsType<ActionResult<InternalEmployeeDto>>(result);
+			var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+			Assert.IsType<SerializableError>(badRequestResult.Value);
+		}
+	}
+}
