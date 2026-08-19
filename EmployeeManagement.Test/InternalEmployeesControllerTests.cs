519bdafc85728e155d4f3d4c695e14ade1cfd3e2
Lawrence LCI <lcheunglci@gmail.com>
2026-08-18 23:39:51 -0700

    Fix MapperConfiguration syntax; add unit test for mapping
    Fixed missing parentheses in AddProfile<EmployeeProfile>().
    Added unit test to verify correct mapping of first InternalEmployee to InternalEmployeeDto.

--- EmployeeManagement.Test/InternalEmployeesControllerTests.cs
+++ EmployeeManagement.Test/InternalEmployeesControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Text;
 using AutoMapper;
 using EmployeeManagement.Business;
@@ -27,7 +28,7 @@ namespace EmployeeManagement.Test
 					new InternalEmployee( "Jane", "Smith", 3, 3400, true, 1 )
 				});
 
-			var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<EmployeeProfile>, new LoggerFactory());
+			var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<EmployeeProfile>(), new LoggerFactory());
 			var mapper = new Mapper(mapperConfiguration);
 
 			var controller = new InternalEmployeesController(employeeServiceMock.Object, mapper);
@@ -39,10 +40,46 @@ namespace EmployeeManagement.Test
 			var actionResult = Assert.IsType<ActionResult<IEnumerable<InternalEmployeeDto>>>(result);
 			var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
 			var dtos = Assert.IsAssignableFrom<IEnumerable<InternalEmployeeDto>>(okObjectResult.Value);
-			Assert.Equal(2, dtos.Count());)
+			Assert.Equal(2, dtos.Count());
+		}
 
+		[Fact]
+		public async Task GetInteralEmployees_GetAction_MustReturnsCorrectlyMappedFirstEmployee()
+		{
+			// Arrange
+			var firstEmployee = new InternalEmployee("John", "Doe", 2, 3000, false, 2)
+			{
+				Id = Guid.Parse("bfdd0acd-d314-491c-b6fa-0e1781e73700"),
+				SuggestedBonus = 400
+			};
 
+			var employeeServiceMock = new Mock<IEmployeeService>();
+			employeeServiceMock
+				.Setup(m => m.FetchInternalEmployeesAsync())
+				.ReturnsAsync(new List<InternalEmployee>() {
+					firstEmployee,
+				});
 
+			var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<EmployeeProfile>(), new LoggerFactory());
+			var mapper = new Mapper(mapperConfiguration);
+
+			var controller = new InternalEmployeesController(employeeServiceMock.Object, mapper);
+
+			// Act
+			var result = await controller.GetInternalEmployees();
+
+			// Assert
+			var actionResult = Assert.IsType<ActionResult<IEnumerable<InternalEmployeeDto>>>(result);
+			var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+			var dtos = Assert.IsAssignableFrom<IEnumerable<InternalEmployeeDto>>(okObjectResult.Value);
+			var firstDto = dtos.First();
+			
+			Assert.Equal(firstEmployee.Id, firstDto.Id);
+			Assert.Equal(firstEmployee.FirstName, firstDto.FirstName);
+			Assert.Equal(firstEmployee.LastName, firstDto.LastName);
+			Assert.Equal(firstEmployee.Salary, firstDto.Salary);
+			Assert.Equal(firstEmployee.SuggestedBonus, firstDto.SuggestedBonus);
+			Assert.Equal(firstEmployee.YearsInService, firstDto.YearsInService);
 		}
 	}
 }
