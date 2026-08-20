657c716bd7ac629ffe4129f22353b48ea6f81a3f
Lawrence LCI <lcheunglci@gmail.com>
2026-08-19 22:59:42 -0700

    Add unit test for admin redirect in controller
    Added GetProtectedInternalEmployee_UserIsAdmin_MustRedirectToProtectedController test to DemoInternalEmployeesControllerTests.cs. Verifies that users with the "Admin" role are redirected to the "GetInternalEmployees" action of the "ProtectedInternalEmployees" controller. Sets up ClaimsPrincipal and configures HttpContext for the test.

--- EmployeeManagement.Test/DemoInternalEmployeesControllerTests.cs
+++ EmployeeManagement.Test/DemoInternalEmployeesControllerTests.cs
@@ -1,8 +1,10 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
 using EmployeeManagement.Business;
 using EmployeeManagement.Controllers;
 using EmployeeManagement.MapperProfiles;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -29,6 +31,40 @@ namespace EmployeeManagement.Test
 			var actionResult = Assert.IsType<ActionResult<InternalEmployeeDto>>(result);
 			var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
 			Assert.IsType<SerializableError>(badRequestResult.Value);
+		}
+
+		[Fact]
+		public async Task GetProtectedInternalEmployee_UserIsAdmin_MustRedirectToProtectedController()
+		{
+			// Arrange
+			var employeeServiceMock = new Mock<IEmployeeService>();
+			var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<EmployeeProfile>(), new LoggerFactory());
+
+			var mapper = new Mapper(mapperConfiguration);
+
+			var controller = new DemoInternalEmployeesController(employeeServiceMock.Object, mapper);
+
+			var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>()
+			{
+				new Claim(ClaimTypes.Name, "Kevin"),
+				new Claim(ClaimTypes.Role, "Admin")
+			}, "TestAuthentication"));
+
+			controller.ControllerContext = new ControllerContext()
+			{
+				HttpContext = new DefaultHttpContext()
+				{
+					User = claimsPrincipal
+				}
+			};
+
+			// Act
+			var result = controller.GetProtectedInternalEmployees();
+
+			// Assert
+			var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("GetInternalEmployees", redirectResult.ActionName);
+			Assert.Equal("ProtectedInternalEmployees", redirectResult.ControllerName);
 		}
 	}
 }
