09cf0d84ff5aaa9d9b054d40a2cbc474d4bab636
Lawrence LCI <lcheunglci@gmail.com>
2026-08-20 22:12:52 -0700

    Add unit test for ineligible promotion returns BadRequest
    Added CreatePromotion_EmployeeNotIsEligible_MustReturnBadRequest test to PromotionsControllerTests. Verifies controller returns BadRequestObjectResult when employee is not eligible for promotion using mocked dependencies.

--- EmployeeManagement.Test/PromotionsControllerTests.cs
+++ EmployeeManagement.Test/PromotionsControllerTests.cs
@@ -15,7 +15,7 @@ namespace EmployeeManagement.Test
 	public class PromotionsControllerTests
 	{
 		[Fact]
-		public async Task TestMethod1()
+		public async Task CreatePromotion_EmployeeIsEligible_MustReturnOkWithIncrementedJobLevel()
 		{
 			// Arrange
 			Guid employeeId = Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb");
@@ -55,6 +55,45 @@ namespace EmployeeManagement.Test
 			Assert.Equal(employeeId, promotionResult.EmployeeId);
 			Assert.Equal(3, promotionResult.JobLevel);
 
+		}
+
+		[Fact]
+		public async Task CreatePromotion_EmployeeNotIsEligible_MustReturnBadRequest()
+		{
+			// Arrange
+			Guid employeeId = Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb");
+
+			var internalEmployee = new InternalEmployee("Megan", "Jones", 2, 3000, false, 2)
+			{
+				Id = employeeId,
+				AttendedCourses = new List<Course>()
+				{
+					new Course("A course"),
+					new Course("Another course")
+				}
+			};
+
+			var employeeServiceMock = new Mock<IEmployeeService>();
+
+			employeeServiceMock.Setup(m => m.FetchInternalEmployeeAsync(employeeId))
+				.ReturnsAsync(internalEmployee);
+
+			var httpClient = new HttpClient(
+				new TestablePromotionEligibilityHandler(false));
+			var employeeManagementRepositoryMock = new Mock<IEmployeeManagementRepository>();
+			employeeManagementRepositoryMock.Setup(m => m.SaveChangesAsync())
+				.Returns(Task.CompletedTask);
+
+			var promotionService = new PromotionService(
+				httpClient, employeeManagementRepositoryMock.Object);
+
+			var controller = new PromotionsController(employeeServiceMock.Object, promotionService);
+
+			// Act
+			var result = await controller.CreatePromotion(new PromotionForCreationDto() { EmployeeId = employeeId });
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
 		}
 	}
 }
