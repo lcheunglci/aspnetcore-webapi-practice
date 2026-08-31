5fd9c10446231d1c13511cbff7bdf154f1a1b35b
Lawrence LCI <lcheunglci@gmail.com>
2026-08-30 19:03:54 -0700

    test: add middleware test for the security header and x-content-type=options

---
+++ EmployeeManagement.Test/MiddlewareTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeManagement.Middleware;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeManagement.Test
+{
+	public class MiddlewareTests
+	{
+
+		[Fact]
+		public async Task SecurityHeaderMiddleware_InvokeAsync_MustAddContentSecurityPolicyHeader()
+		{
+			// Arrange
+			var httpContext = new DefaultHttpContext();
+			RequestDelegate next = (HttpContext ctx) => Task.CompletedTask;
+			var middleware = new EmployeeManagementSecurityHeadersMiddleware(next);
+
+			// Act
+			await middleware.InvokeAsync(httpContext);
+
+			// Assert
+			Assert.True(httpContext.Response.Headers.ContainsKey("Content-Security-Policy"));
+			Assert.Equal("default-src 'self';frame-ancestors 'none'",
+				httpContext.Response.Headers.ContentSecurityPolicy.ToString());
+		}
+
+		[Fact]
+		public async Task SecurityHeaderMiddleware_InvokeAsync_MustAddXContentTypeOptionsHeader()
+		{
+			// Arrange
+			var httpContext = new DefaultHttpContext();
+			RequestDelegate next = (HttpContext ctx) => Task.CompletedTask;
+			var middleware = new EmployeeManagementSecurityHeadersMiddleware(next);
+
+			// Act
+			await middleware.InvokeAsync(httpContext);
+
+			// Assert
+			Assert.True(httpContext.Response.Headers.ContainsKey("X-Content-Type-Options"));
+			Assert.Equal("nosniff",
+				httpContext.Response.Headers.XContentTypeOptions.ToString());
+		}
+	}
+}
