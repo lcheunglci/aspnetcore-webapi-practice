7905ecc9b08f4075d935d8ba70ad6f1f91e9569f
Lawrence LCI <lcheunglci@gmail.com>
2026-08-31 22:38:02 -0700

    test: add test to service registration

---
+++ EmployeeManagement.Test/ServiceRegistrationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagement.Business;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Internal;
+
+namespace EmployeeManagement.Test
+{
+	public class ServiceRegistrationTests
+	{
+		[Fact]
+		public void RegisterBusinessServices_MustResolveEmplyeeService()
+		{
+			// Arrange
+			var services = new ServiceCollection();
+			services.RegisterBusinessServices();
+
+			var configuration = new ConfigurationBuilder()
+				.AddInMemoryCollection(new Dictionary<string, string?>
+				{
+					{ "ConnectionStrings:EmployeeManagementDB", "DataSource=:memory:" }
+				})
+				.Build();
+
+			services.RegisterDataServices(configuration);
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			// Act
+			var employeeService = serviceProvider.GetService<IEmployeeService>();
+
+			// Assert
+			Assert.NotNull(employeeService);
+			Assert.IsType<EmployeeService>(employeeService);
+		}
+	}
+}
