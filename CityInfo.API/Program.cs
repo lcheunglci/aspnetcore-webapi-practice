71775d91bcf0a8d2a007adb27cf04694b37288c7
Lawrence LCI <lcheunglci@gmail.com>
2026-09-22 20:16:51 -0700

    feat: add application insight logging for prod

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Asp.Versioning;
 using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
@@ -12,14 +13,39 @@ using Serilog;
 Log.Logger = new LoggerConfiguration()
 	.MinimumLevel.Debug()
 	.WriteTo.Console()
-	.WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+	// .WriteTo.ApplicationInsights()
+	// .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
 	.CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Logging.ClearProviders();
 //builder.Logging.AddConsole();
 
-builder.Host.UseSerilog();
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+if (environment == Environments.Development)
+{
+	builder.Host.UseSerilog(
+		(context, loggerConfiguration) => loggerConfiguration
+		.MinimumLevel.Debug()
+		.WriteTo.Console()
+		.WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+		);
+}
+else
+{
+	builder.Host.UseSerilog(
+		(context, loggerConfiguration) => loggerConfiguration
+		.MinimumLevel.Debug()
+		.WriteTo.Console()
+		.WriteTo.ApplicationInsights(new TelemetryConfiguration
+		{
+			InstrumentationKey = builder.Configuration["ApplicationInsightsInstrumentationKey"]
+		}, TelemetryConverter.Traces)
+		.WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+		);
+}
+
+//builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddControllers()
